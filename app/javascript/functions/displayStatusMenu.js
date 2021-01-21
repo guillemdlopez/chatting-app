dbaa5ef75e021f4478f4e03b05c264e0b07411ea
Guillem Delas <guillemdelas@hotmail.com>
2021-01-21 20:38:12 +0100

    improved the displayDropdown function js

--- app/javascript/functions/displayStatusMenu.js
+++ app/javascript/functions/displayStatusMenu.js
@@ -3,11 +3,18 @@
 const displayStatusMenu = () => {
   const dropdownMenu = document.querySelector(".dropdown-user-status");
   const userInfo = document.querySelector(".user-info");
-  //   console.log(settingsBtn);
+
   if (dropdownMenu) {
     userInfo.addEventListener("click", (e) => {
       if (e.target.closest(".settings")) {
         dropdownMenu.classList.toggle("totally-hidden");
+        dropdownMenu.style.transition = "0.3s";
+      }
+    });
+
+    document.addEventListener("click", function (e) {
+      if (!e.target.closest(".settings")) {
+        dropdownMenu.classList.add("totally-hidden");
         dropdownMenu.style.transition = "0.3s";
       }
     });
